c8afa3d0cb677da3e7075c54e1c6ea3498e30424
zhuiyishanran <awaitm@qq.com>
2018-04-21 21:22:48 +0800

    static

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -101,6 +101,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/home/zhuiyishanran/my-first-blog/static"
+STATIC_ROOT = "/home/zhuiyishanran/my-first-blog/mysite/static"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
